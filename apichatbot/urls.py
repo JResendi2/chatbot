97fac74cffa06249ef6f4251ad67063c1f5c80af
JResendi2 <resendizj607@gmail.com>
2024-07-04 13:24:46 -0600

    actions

---
+++ apichatbot/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import APIChatbot;
+from . import views;
+
+
+urlpatterns = [
+    path('', APIChatbot.as_view(), name="chat"),
+    path('create', views.createCredentials, name="create"),
+    path('test', views.test, name="test"),
+
+]
+   
